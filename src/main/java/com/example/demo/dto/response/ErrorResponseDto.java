9e893e1303c6aebb74064aa816e2c8612f6ea177
viswanathans3005 <viswanathans3005@gmail.com>
2026-09-22 16:39:38 +0530

    adding proper exception output

---
+++ src/main/java/com/example/demo/dto/response/ErrorResponseDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto.response;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponseDto {
+
+	private int status;
+	private String message;
+	private LocalDateTime timestamp;
+}
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,28 +1,45 @@
 package com.example.demo.exception;
 
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.demo.dto.response.ErrorResponseDto;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomerAlreadyExistsException.class)
-	public ResponseEntity<String> handleCustomerAlreadyException( CustomerAlreadyExistsException ex){
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyException( CustomerAlreadyExistsException ex){
+		ErrorResponseDto eRes = new ErrorResponseDto();
+		eRes.setStatus(HttpStatus.CONFLICT.value());
+		eRes.setMessage(ex.getMessage());
+		eRes.setTimestamp(LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(eRes);
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex){
+	public ResponseEntity<ErrorResponseDto> handleValidationException(MethodArgumentNotValidException ex){
 		String message = ex.getBindingResult().getFieldError().getDefaultMessage();
-	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+		ErrorResponseDto eRes = new ErrorResponseDto();
+		eRes.setStatus(HttpStatus.BAD_REQUEST.value());
+		eRes.setMessage(message);
+		eRes.setTimestamp(LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(eRes);
 	}
 	
 	@ExceptionHandler(CustomerNotFoundException.class)
-	public ResponseEntity<String> handleCustomerNotAvailable(CustomerNotFoundException ex){
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	public ResponseEntity<ErrorResponseDto> handleCustomerNotAvailable(CustomerNotFoundException ex){
+		ErrorResponseDto eResponse = new ErrorResponseDto();
+		eResponse.setStatus(HttpStatus.NOT_FOUND.value());
+		eResponse.setMessage(ex.getMessage());
+		eResponse.setTimestamp(LocalDateTime.now());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(eResponse);
+		
 	}
 	
 }
